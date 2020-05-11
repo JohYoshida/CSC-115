4c2c33e7096d8e91aafc0731abfd1794e2f69572
Joh Yoshida <johyoshida@gmail.com>
2020-05-11 14:15:36 -0700

    Prettier

--- Assignment 1/ArrayOperations.java
+++ Assignment 1/ArrayOperations.java
@@ -1,6 +1,6 @@
 /*
-	Joh Yoshida
-	V00798535
+ *      Joh Yoshida
+ *      V00798535
  */
 
 import java.lang.Math;
@@ -24,188 +24,206 @@ import java.lang.Math;
  *
  */
 public class ArrayOperations {
+  /*
+   * printArray
+   *
+   * Purpose: prints all values in the array to the console
+   *          Example format:  {1,2,3,4}
+   *
+   * Parameters: int[] - an array of integers
+   *
+   * Returns: void
+   *
+   */
+  public static void printArray(int[] array) {
+    System.out.print("{");
+    for (int i = 0; i < array.length; i++)
+    {
+      System.out.print(array[i]);
+      if (i < array.length - 1)
+      {
+        System.out.print(",");
+      }
+    }
+    System.out.println("}");
+  }
 
+  /*
+   * sumArray
+   *
+   * Purpose: computes the sum of all values in the given array
+   *
+   * Parameters: int[] - an array of integers
+   *
+   * Returns: int - sum of all values in the array
+   *
+   */
+  public static int sumArray(int[] array) {
+    int sum = 0;
 
-    /*
-     * printArray
-     *
-     * Purpose: prints all values in the array to the console
-     *          Example format:  {1,2,3,4}
-     *
-     * Parameters: int[] - an array of integers
-     *
-     * Returns: void
-     *
-     */
-	public static void printArray ( int[] array ) {
-		System.out.print("{");
-		for(int i=0; i<array.length; i++) {
-			System.out.print(array[i]);
-			if(i<array.length-1) {
-				System.out.print(",");
-			}
-		}
-		System.out.println("}");
-	}
-
-	/*
-     * sumArray
-     *
-     * Purpose: computes the sum of all values in the given array
-     *
-     * Parameters: int[] - an array of integers
-     *
-     * Returns: int - sum of all values in the array
-     *
-     */
-    public static int sumArray ( int[] array ) {
-        int sum = 0;
-
-        for(int i=0; i<array.length; i++)
-            sum += array[i];
-
-        return sum;
+    for (int i = 0; i < array.length; i++)
+    {
+      sum += array[i];
     }
 
-    /*
-     * productArray
-     *
-     * Purpose: computes the product of all values in the given array
-     *  NOTE: product of 3 numbers n1, n2 and n3 = n1*n2*n3
-     *  NOTE: product of no numbers = 1
-     *
-     * Parameters: int[] - an array of integers
-     *
-     * Returns: int - product of all values in the array
-     *
-     */
-    public static int productArray ( int[] array ) {
-        int product = 1;
+    return sum;
+  }
 
-				for (int i = 0; i < array.length; i++) {
-					product = product * array[i];
-				}
+  /*
+   * productArray
+   *
+   * Purpose: computes the product of all values in the given array
+   *  NOTE: product of 3 numbers n1, n2 and n3 = n1*n2*n3
+   *  NOTE: product of no numbers = 1
+   *
+   * Parameters: int[] - an array of integers
+   *
+   * Returns: int - product of all values in the array
+   *
+   */
+  public static int productArray(int[] array) {
+    int product = 1;
 
-        return product;
+    for (int i = 0; i < array.length; i++)
+    {
+      product = product * array[i];
     }
 
-    /*
-     * arrayMax
-     *
-     * Purpose: finds the maximum value in the given array
-     *
-     * Parameters: int[] - an array of integers
-     *
-     * Preconditions: array contains at least one element
-     *
-     * Returns: int - maximum value in the array
-     *
-     */
-		public static int arrayMax ( int[] array ) {
-			int max = array[0];
-			for (int i = 1; i < array.length; i++) {
-				if (array[i] > max) {
-					max = array[i];
-				}
-			}
-			return max;
-		}
+    return product;
+  }
 
+  /*
+   * arrayMax
+   *
+   * Purpose: finds the maximum value in the given array
+   *
+   * Parameters: int[] - an array of integers
+   *
+   * Preconditions: array contains at least one element
+   *
+   * Returns: int - maximum value in the array
+   *
+   */
+  public static int arrayMax(int[] array) {
+    int max = array[0];
 
-    /*
-     * arrayMin
-     *
-     * Purpose: finds the minimum value in the given array
-     *
-     * Parameters: int[] - an array of integers
-     *
-     * Preconditions: array contains at least one element
-     *
-     * Returns:  int - minimum value in the array
-     *
-     */
-    	public static int arrayMin ( int[] array ) {
-				int min = array[0];
-				for (int i = 1; i < array.length; i++) {
-					if (array[i] < min) {
-						min = array[i];
-					}
-				}
-				return min;
-		}
+    for (int i = 1; i < array.length; i++)
+    {
+      if (array[i] > max)
+      {
+        max = array[i];
+      }
+    }
+    return max;
+  }
 
+  /*
+   * arrayMin
+   *
+   * Purpose: finds the minimum value in the given array
+   *
+   * Parameters: int[] - an array of integers
+   *
+   * Preconditions: array contains at least one element
+   *
+   * Returns:  int - minimum value in the array
+   *
+   */
+  public static int arrayMin(int[] array) {
+    int min = array[0];
 
+    for (int i = 1; i < array.length; i++)
+    {
+      if (array[i] < min)
+      {
+        min = array[i];
+      }
+    }
+    return min;
+  }
 
-    /*
-     * allBelowValue
-     *
-     * Purpose: determines whether all values in array are below a threshold
-     *
-     * Parameters: int[] - array of integers
-     *             int - threshold that numbers must be below
-     *
-     * Returns: boolean - true if all values below, false otherwise
-     *
-     */
-     public static boolean allBelowValue ( int[] array, int threshold ) {
-			 for (int i = 0; i < array.length; i++) {
-			 		if (array[i] >= threshold) {
-						return false;
-					}
-			 }
-			 return true;
-		 }
+  /*
+   * allBelowValue
+   *
+   * Purpose: determines whether all values in array are below a threshold
+   *
+   * Parameters: int[] - array of integers
+   *             int - threshold that numbers must be below
+   *
+   * Returns: boolean - true if all values below, false otherwise
+   *
+   */
+  public static boolean allBelowValue(int[] array, int threshold) {
+    for (int i = 0; i < array.length; i++)
+    {
+      if (array[i] >= threshold)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    /*
-     * containsValue
-     *
-     * Purpose: determines whether the array contains the given value
-     *
-     * Parameters: int[] - array of integers to search through
-     *             int - value to search for
-     *
-     * Returns: boolean - true if value is found, false otherwise
-     *
-     */
-    public static boolean containsValue ( int[] array, int value ) {
-			for (int i = 0; i < array.length; i++) {
-				if (array[i] == value) {
-					return true;
-				}
-			}
-			return false;
-		}
+  /*
+   * containsValue
+   *
+   * Purpose: determines whether the array contains the given value
+   *
+   * Parameters: int[] - array of integers to search through
+   *             int - value to search for
+   *
+   * Returns: boolean - true if value is found, false otherwise
+   *
+   */
+  public static boolean containsValue(int[] array, int value) {
+    for (int i = 0; i < array.length; i++)
+    {
+      if (array[i] == value)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
 
+  /*
+   * containsArray
+   *
+   * Purpose: determines whether the array searchArray contains
+   *          all of the values in array nums in the same order
+   *
+   * Parameters: int[] nums - array of integers that must be found
+   *             int[] searchArray - array of integers to search through
+   *
+   * Returns: boolean - true if all values in nums found, false otherwise
+   *
+   */
+  public static boolean containsArray(int[] nums, int[] searchArray) {
+    int index = 0;
 
-	/*
-     * containsArray
-     *
-     * Purpose: determines whether the array searchArray contains
-	 	 *          all of the values in array nums in the same order
-     *
-     * Parameters: int[] nums - array of integers that must be found
-     *             int[] searchArray - array of integers to search through
-     *
-     * Returns: boolean - true if all values in nums found, false otherwise
-     *
-     */
-		public static boolean containsArray (int[] nums, int[] searchArray) {
-			int index = 0;
-			for (int i = 0; i < searchArray.length; i++) {
-				if (searchArray[i] == nums[index]) {
-					if (index < nums.length - 1) {
-						index++;
-					} else if (index == nums.length - 1) {
-						System.out.println("");
-						return true;
-					}
-				} else if (searchArray[i] == nums[0]) {
-					index = 1;
-				} else {
-					index = 0;
-				}
-			}
-			return false;
-		}
-
+    for (int i = 0; i < searchArray.length; i++)
+    {
+      if (searchArray[i] == nums[index])
+      {
+        if (index < nums.length - 1)
+        {
+          index++;
+        }
+        else if (index == nums.length - 1)
+        {
+          System.out.println("");
+          return true;
+        }
+      }
+      else if (searchArray[i] == nums[0])
+      {
+        index = 1;
+      }
+      else
+      {
+        index = 0;
+      }
+    }
+    return false;
+  }
 }
