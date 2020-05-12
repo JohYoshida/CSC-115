d54c4d625c844951d395528632e353b427400dc7
Joh Yoshida <johyoshida@gmail.com>
2020-05-12 12:00:18 -0700

    Fixed isSorted

--- Lab 1/Lab1.java
+++ Lab 1/Lab1.java
@@ -112,11 +112,8 @@ public class Lab1 {
    */
   public static boolean isSorted(int[] array) {
     for (int i = 0; i < array.length - 1; i++) {
-      if (array[i + 1] <= array[i]) {
+      if (array[i + 1] < array[i]) {
         return false;
-      }
-      else{
-        return true;
       }
     }
 
