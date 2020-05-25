a8ea0f33776ed1ab88ddb12d5222d5b4f2a03fb5
Joh Yoshida <johyoshida@gmail.com>
2020-05-25 12:58:20 -0700

    Initial commit

---
+++ May 25/IntegerArrayList.java
@@ -0,0 +1,48 @@
+public class IntegerArrayList implements IntegerList {
+
+	private static final int INITIAL_SIZE = 10;
+
+	private int[] data;
+	private int numElements;
+
+	public IntegerArrayList() {
+		data = new int[INITIAL_SIZE];
+		numElements = 0;
+	}
+
+	public void addFront(int val) {
+		
+	}
+	
+	public void addBack(int val) {
+		data[numElements] = val;
+		numElements++;
+	}
+	
+	public void insertAt(int position, int val) {
+		
+	}
+	
+	public int size() {
+		return numElements;
+	}
+	
+	public int get(int position) {
+		return 0;
+	}
+
+	/* Parameters: nothing
+	 * Purpose: create a string representation of list
+	 * Returns: (String) the string representation
+	 */
+	public String toString() {
+		String s = "List contents:";
+
+		for(int i=0; i<numElements; i++) {
+			s += " " + data[i];
+		}
+
+		return s;
+	}
+
+}
