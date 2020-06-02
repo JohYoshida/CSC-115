6add4568e23f103f39f31a86f3ecd6777d7bc249
Joh Yoshida <johyoshida@gmail.com>
2020-06-02 12:07:36 -0700

    Added class skeleton

---
+++ Lab 4/StudentLinkedList.java
@@ -0,0 +1,83 @@
+/*
+ * Joh Yoshida
+ * V00798535
+ */
+
+/*
+ * StudentLinkedList.java
+ *
+ * An implementation of the StudentList class
+ *
+ */
+public class StudentLinkedList implements StudentList {
+  private StudentNode head;
+  private int count;
+
+  public StudentLinkedList() {
+    head  = null;
+    count = 0;
+  }
+
+  /*
+   *
+   * Purpose: Adds a node the to back of the list
+   *
+   * Parameters: Student - the data for the new node
+   *
+   * Returns: void
+   *
+   */
+  public void add(Student student) {
+  }
+
+  /*
+   *
+   * Purpose: Returns the size of the linked list
+   *
+   * Parameters: none
+   *
+   * Returns: int - the size
+   *
+   */
+  public int size() {
+    return 0;
+  }
+
+  /*
+   *
+   * Purpose: Creates a string representation of the linked list
+   *
+   * Parameters: none
+   *
+   * Returns: String - the string representation
+   *
+   */
+  public String toString() {
+    return null;
+  }
+
+  /*
+   *
+   * Purpose: Removes the front node of the list
+   *
+   * Parameters: none
+   *
+   * Returns: void
+   *
+   */
+  public void removeFront() {
+  }
+
+  /*
+   *
+   * Purpose:
+   *
+   * Parameters:
+   *
+   * Returns:
+   *
+   */
+  public boolean contains(Student student) {
+    return false;
+  }
+}
