10e9d3af76d7fe9d097aa28a2f26f8735e351db7
Joh Yoshida <johyoshida@gmail.com>
2020-06-02 11:49:07 -0700

    Added class skeleton

---
+++ Lab 4/StudentNode.java
@@ -0,0 +1,72 @@
+/*
+ * Joh Yoshida
+ * V00798535
+ */
+
+/*
+ * StudentNode.java
+ *
+ * A StudentNode class
+ *
+ */
+
+public class StudentNode {
+  public StudentNode next;
+  private Student data;
+
+  public StudentNode(Student data) {
+  }
+
+  public StudentNode(Student data, StudentNode next) {
+  }
+
+  /*
+   *
+   * Purpose: Get the next node.
+   *
+   * Parameters: none
+   *
+   * Returns: StudentNode - the next node
+   *
+   */
+  public StudentNode getNext() {
+    return null;
+  }
+
+  /*
+   *
+   * Purpose: Set the next node
+   *
+   * Parameters: StudentNode - the new next node
+   *
+   * Returns: void
+   *
+   */
+  public void setNext(StudentNode next) {
+  }
+
+  /*
+   *
+   * Purpose: Gets the data in this node
+   *
+   * Parameters: none
+   *
+   * Returns: Student - the data in the node
+   *
+   */
+  public Student getData() {
+    return null;
+  }
+
+  /*
+   *
+   * Purpose: Sets the data in this node
+   *
+   * Parameters: Student - the new data to be set
+   *
+   * Returns: void
+   *
+   */
+  public void setData(Student data) {
+  }
+}
