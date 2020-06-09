6a7e35980457568a9e7932c34c0f029203cf82e8
Joh Yoshida <johyoshida@gmail.com>
2020-06-09 12:36:38 -0700

    Tested isAscending

--- Lab 5/Lab5Tester.java
+++ Lab 5/Lab5Tester.java
@@ -13,7 +13,8 @@ public class Lab5Tester {
       testDoubleOddsRecursive();
       testSumValues();
       testMultiplyValues();
-      // testDoubleAtOddPositions();
+      testDoubleAtOddPositions();
+      testIsAscending();
     } catch (Exception e) {
       System.out.println("Your code threw an Exception.");
       System.out.println("Perhaps a stack trace will help:");
@@ -108,25 +109,25 @@ public class Lab5Tester {
   }
 
   public static void testMultiplyValues() {
-    IntegerLinkedList emptyList    = new IntegerLinkedList();
-    IntegerLinkedList list3        = new IntegerLinkedList();
-    IntegerLinkedList anotherList3 = new IntegerLinkedList();
+    IntegerLinkedList emptyList = new IntegerLinkedList();
+    IntegerLinkedList list3a    = new IntegerLinkedList();
+    IntegerLinkedList list3b    = new IntegerLinkedList();
 
-    list3.addFront(-2);
-    list3.addFront(0);
-    list3.addFront(7);
-    anotherList3.addFront(-2);
-    anotherList3.addFront(3);
-    anotherList3.addFront(7);
+    list3a.addFront(-2);
+    list3a.addFront(0);
+    list3a.addFront(7);
+    list3b.addFront(-2);
+    list3b.addFront(3);
+    list3b.addFront(7);
 
     int result = emptyList.product();
-    displayResults(result == 1, "testSumValues - empty");
+    displayResults(result == 1, "testMultiplyValues - empty");
 
-    result = list3.product();
-    displayResults(result == 0, "testSumValues - length3");
+    result = list3a.product();
+    displayResults(result == 0, "testMultiplyValues - length3");
 
-    result = anotherList3.product();
-    displayResults(result == -42, "testSumValues - length3");
+    result = list3b.product();
+    displayResults(result == -42, "testMultiplyValues - length3");
   }
 
   public static void testDoubleAtOddPositions() {
@@ -143,6 +144,52 @@ public class Lab5Tester {
 
     list3a.doubleOddPositionValues();
     displayResults(list3a.toString().equals("8 14 5 -4"), "testDoubleAtOddPositions - length3");
+  }
+
+  public static void testIsAscending() {
+    IntegerLinkedList emptyList = new IntegerLinkedList();
+    IntegerLinkedList list3a    = new IntegerLinkedList();
+    IntegerLinkedList list3b    = new IntegerLinkedList();
+    IntegerLinkedList list5a    = new IntegerLinkedList();
+    IntegerLinkedList list5b    = new IntegerLinkedList();
+
+    list3a.addFront(7);
+    list3a.addFront(0);
+    list3a.addFront(-2);
+    // System.out.println(list3a.toString());
+    list3b.addFront(7);
+    list3b.addFront(8);
+    list3b.addFront(-2);
+    // System.out.println(list3b.toString());
+
+    list5a.addFront(5);
+    list5a.addFront(4);
+    list5a.addFront(3);
+    list5a.addFront(2);
+    list5a.addFront(1);
+    // System.out.println(list5a.toString());
+
+    list5b.addFront(0);
+    list5b.addFront(4);
+    list5b.addFront(3);
+    list5b.addFront(2);
+    list5b.addFront(1);
+    // System.out.println(list5b.toString());
+
+    boolean result = emptyList.isAscending();
+    displayResults(result == true, "testIsAscending - empty");
+
+    result = list3a.isAscending();
+    displayResults(result == true, "testIsAscending - length3");
+
+    result = list3b.isAscending();
+    displayResults(result == false, "testIsAscending - length3");
+
+    result = list5a.isAscending();
+    displayResults(result == true, "testIsAscending - length5");
+
+    result = list5b.isAscending();
+    displayResults(result == false, "testIsAscending - length5");
   }
 
   public static void displayResults(boolean passed, String testName) {
