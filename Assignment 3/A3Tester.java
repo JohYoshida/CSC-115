59e90a141f5843b81647fc4d07d3c45b033bd0a7
Joh Yoshida <johyoshida@gmail.com>
2020-06-09 11:01:03 -0700

    Tests

--- Assignment 3/A3Tester.java
+++ Assignment 3/A3Tester.java
@@ -27,6 +27,7 @@ public class A3Tester {
   }
 
   public static void testAddFront() {
+    System.out.println("testAddFront");
     String result      = "";
     A3LinkedList list1 = new A3LinkedList();
 
@@ -47,127 +48,218 @@ public class A3Tester {
   }
 
   public static void testAddBack() {
-    // String result = "";
-    // A3LinkedList list1 = new A3LinkedList();
+    System.out.println("testAddBack");
+    String result      = "";
+    A3LinkedList list1 = new A3LinkedList();
 
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{}"), "testAddBack");
+    result = list1.frontToBack();
+    displayResults(result.equals("{}"), "testAddBack");
 
-    // list1.addBack("F");
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{F}"), "testAddBack");
+    list1.addBack("F");
+    result = list1.frontToBack();
+    displayResults(result.equals("{F}"), "testAddBack");
 
-    // list1.addBack("U");
-    // list1.addBack("N");
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{FUN}"), "testAddBack");
-    // result = list1.backToFront();
-    // displayResults(result.equals("{NUF}"), "testAddBack");
+    list1.addBack("U");
+    list1.addBack("N");
+    result = list1.frontToBack();
+    displayResults(result.equals("{FUN}"), "testAddBack");
+    result = list1.backToFront();
+    displayResults(result.equals("{NUF}"), "testAddBack");
   }
 
   public static void testSizeAndIsEmpty() {
-    // int result = 0;
-    // A3LinkedList list1 = new A3LinkedList();
+    System.out.println("testSizeAndIsEmpty");
+    int result         = 0;
+    A3LinkedList list1 = new A3LinkedList();
 
-    // result = list1.size();
-    // displayResults(result==0, "testSizeAndIsEmpty");
-    // displayResults(list1.isEmpty()==true, "testSizeAndIsEmpty");
+    result = list1.size();
+    displayResults(result == 0, "testSizeAndIsEmpty");
+    displayResults(list1.isEmpty() == true, "testSizeAndIsEmpty");
 
-    // list1.addFront("C");
-    // list1.addFront("S");
-    // list1.addFront("C");
-    // result = list1.size();
-    // displayResults(result==3, "testSizeAndIsEmpty");
-    // displayResults(list1.isEmpty()==false, "testSizeAndIsEmpty");
+    list1.addFront("C");
+    list1.addFront("S");
+    list1.addFront("C");
+    result = list1.size();
+    displayResults(result == 3, "testSizeAndIsEmpty");
+    displayResults(list1.isEmpty() == false, "testSizeAndIsEmpty");
 
-    // list1.addBack("115");
-    // result = list1.size();
-    // displayResults(result==4, "testSizeAndIsEmpty");
+    list1.addBack("115");
+    result = list1.size();
+    displayResults(result == 4, "testSizeAndIsEmpty");
   }
 
   public static void testRemoveFront() {
-    // String result = "";
-    // A3LinkedList list1 = new A3LinkedList();
+    System.out.println("testRemoveFront");
 
-    // list1.addBack("P");
-    // list1.addBack("I");
-    // list1.addBack("N");
-    // list1.addBack("K");
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{PINK}"), "testRemoveFront");
+    String result      = "";
+    int size           = 0;
+    A3LinkedList list1 = new A3LinkedList();
 
-    // list1.removeFront();
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{INK}"), "testRemoveFront");
+    list1.addBack("P");
+    list1.addBack("I");
+    list1.addBack("N");
+    list1.addBack("K");
+    result = list1.frontToBack();
+    displayResults(result.equals("{PINK}"), "testRemoveFront");
+
+    list1.removeFront();
+    result = list1.frontToBack();
+    displayResults(result.equals("{INK}"), "testRemoveFront");
 
     /* Write additional tests here to ensure all of your pointers
      * have been updated correctly.
      *
      * You should also ensure that your size and isEmpty
      * methods work with removal as well as addition */
+    list1.removeFront();
+    result = list1.frontToBack();
+    displayResults(result.equals("{NK}"), "testRemoveFront");
+
+    size = list1.size();
+    displayResults(size == 2, "testRemoveFront");
+    displayResults(list1.isEmpty() == false, "testRemoveFront");
+
+    list1.removeFront();
+    result = list1.frontToBack();
+    displayResults(result.equals("{K}"), "testRemoveFront");
+
+    size = list1.size();
+    displayResults(size == 1, "testRemoveFront");
+    displayResults(list1.isEmpty() == false, "testRemoveFront");
+
+    list1.removeFront();
+    result = list1.frontToBack();
+    displayResults(result.equals("{}"), "testRemoveFront");
+
+    size = list1.size();
+    displayResults(size == 0, "testRemoveFront");
+    displayResults(list1.isEmpty() == true, "testRemoveFront");
+
+    list1.removeFront();
+    result = list1.frontToBack();
+    displayResults(result.equals("{}"), "testRemoveFront");
+    size = list1.size();
+    displayResults(size == 0, "testRemoveFront");
+    displayResults(list1.isEmpty() == true, "testRemoveFront");
   }
 
   public static void testRemoveBack() {
-    // Write all of your own tests here
+    System.out.println("testRemoveBack");
+
+    String result      = "";
+    int size           = 0;
+    A3LinkedList list1 = new A3LinkedList();
+
+    list1.addBack("P");
+    list1.addBack("I");
+    list1.addBack("N");
+    list1.addBack("K");
+    result = list1.frontToBack();
+    displayResults(result.equals("{PINK}"), "testRemoveBack");
+
+    size = list1.size();
+    displayResults(size == 4, "testRemoveBack");
+    displayResults(list1.isEmpty() == false, "testRemoveBack");
+
+    list1.removeBack();
+    result = list1.frontToBack();
+    displayResults(result.equals("{PIN}"), "testRemoveBack");
+
+    size = list1.size();
+    displayResults(size == 3, "testRemoveBack");
+    displayResults(list1.isEmpty() == false, "testRemoveBack");
+
+    list1.removeBack();
+    result = list1.frontToBack();
+    displayResults(result.equals("{PI}"), "testRemoveBack");
+
+    size = list1.size();
+    displayResults(size == 2, "testRemoveBack");
+    displayResults(list1.isEmpty() == false, "testRemoveBack");
+
+    list1.removeBack();
+    result = list1.frontToBack();
+    displayResults(result.equals("{P}"), "testRemoveBack");
+
+    size = list1.size();
+    displayResults(size == 1, "testRemoveBack");
+    displayResults(list1.isEmpty() == false, "testRemoveBack");
+
+    list1.removeBack();
+    result = list1.frontToBack();
+    displayResults(result.equals("{}"), "testRemoveBack");
+
+    size = list1.size();
+    displayResults(size == 0, "testRemoveBack");
+    displayResults(list1.isEmpty() == true, "testRemoveBack");
+
+    list1.removeBack();
+    result = list1.frontToBack();
+    displayResults(result.equals("{}"), "testRemoveBack");
+
+    size = list1.size();
+    displayResults(size == 0, "testRemoveBack");
+    displayResults(list1.isEmpty() == true, "testRemoveBack");
   }
 
   public static void testRotate() {
-    // String result = "";
-    // A3LinkedList list1 = new A3LinkedList();
+    String result      = "";
+    A3LinkedList list1 = new A3LinkedList();
 
-    // list1.addBack("A");
-    // list1.addBack("B");
-    // list1.addBack("C");
-    // list1.addBack("D");
-    // list1.addBack("E");
-    // list1.addBack("F");
-    // list1.addBack("G");
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{ABCDEFG}"), "testRotate");
+    list1.addBack("A");
+    list1.addBack("B");
+    list1.addBack("C");
+    list1.addBack("D");
+    list1.addBack("E");
+    list1.addBack("F");
+    list1.addBack("G");
+    result = list1.frontToBack();
+    displayResults(result.equals("{ABCDEFG}"), "testRotate");
 
-    // list1.rotate(1);
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{GABCDEF}"), "testRotate");
+    list1.rotate(1);
+    result = list1.frontToBack();
+    displayResults(result.equals("{GABCDEF}"), "testRotate");
 
-    // list1.rotate(3);
-    // result = list1.frontToBack();
-    // displayResults(result.equals("{DEFGABC}"), "testRotate");
+    list1.rotate(3);
+    result = list1.frontToBack();
+    displayResults(result.equals("{DEFGABC}"), "testRotate");
 
     /* Write additional tests here to ensure all of your pointers
      * have been updated correctly. */
   }
 
   public static void testInterleave() {
-    // A3LinkedList list1 = new A3LinkedList();
-    // A3LinkedList list2 = new A3LinkedList();
-    // String result1 = "";
-    // String result2 = "";
+    A3LinkedList list1 = new A3LinkedList();
+    A3LinkedList list2 = new A3LinkedList();
+    String result1     = "";
+    String result2     = "";
 
-    // list1.addBack("A");
-    // list1.addBack("B");
-    // list1.addBack("C");
-    // list1.addBack("D");
-    // list1.addBack("E");
-    // list1.addBack("F");
-    // list1.addBack("G");
-    // result1 = list1.frontToBack();
-    // displayResults(result1.equals("{ABCDEFG}"), "testInterleave");
+    list1.addBack("A");
+    list1.addBack("B");
+    list1.addBack("C");
+    list1.addBack("D");
+    list1.addBack("E");
+    list1.addBack("F");
+    list1.addBack("G");
+    result1 = list1.frontToBack();
+    displayResults(result1.equals("{ABCDEFG}"), "testInterleave");
 
-    // list2.addBack("L");
-    // list2.addBack("M");
-    // list2.addBack("N");
-    // list2.addBack("O");
-    // list2.addBack("P");
-    // list2.addBack("Q");
-    // list2.addBack("R");
-    // result2 = list2.frontToBack();
-    // displayResults(result2.equals("{LMNOPQR}"), "testInterleave");
+    list2.addBack("L");
+    list2.addBack("M");
+    list2.addBack("N");
+    list2.addBack("O");
+    list2.addBack("P");
+    list2.addBack("Q");
+    list2.addBack("R");
+    result2 = list2.frontToBack();
+    displayResults(result2.equals("{LMNOPQR}"), "testInterleave");
 
-    // list1.interleave(list2);
-    // result1 = list1.frontToBack();
-    // result2 = list2.frontToBack();
-    // displayResults(result1.equals("{AMCOEQG}"), "testInterleave");
-    // displayResults(result2.equals("{LBNDPFR}"), "testInterleave");
+    list1.interleave(list2);
+    result1 = list1.frontToBack();
+    result2 = list2.frontToBack();
+    System.out.println(result1 + " " + result2);
+    displayResults(result1.equals("{AMCOEQG}"), "testInterleave");
+    displayResults(result2.equals("{LBNDPFR}"), "testInterleave");
 
     /* Write additional tests here to ensure all of your pointers
      * have been updated correctly. */
