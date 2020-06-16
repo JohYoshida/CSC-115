9627d2185224ac2f92abc5eec349bbaf4df2cfe7
Joh Yoshida <johyoshida@gmail.com>
2020-06-16 11:54:00 -0700

    Implemented StackArrayBased methods

--- Lab 6/Part 1/StackArrayBased.java
+++ Lab 6/Part 1/StackArrayBased.java
@@ -13,25 +13,47 @@ public class StackArrayBased implements Stack {
   }
 
   public int size() {
-    return 0;
+    return top + 1;
   }
 
   public boolean isEmpty() {
-    return true;
+    if (top == -1) {
+      return true;
+    }
+    return false;
   }
 
   public void push(char val) {
+    top++;
+    if (top == data.length) {
+      char[] newData = new char[top + 1];
+      for (int i = 0; i < data.length; i++) {
+        newData[i] = data[i];
+      }
+      data = newData;
+    }
+    data[top] = val;
   }
 
   public char pop() {
-    return 'a';
+    char c = data[top];
+
+    top--;
+    return c;
   }
 
   public char peek() {
-    return 'a';
+    return data[top];
+  }
+
+  public void popAll() {
+    data = new char[INIT_SZ];
+    top  = -1;
   }
 
   public void makeEmpty() {
+    data = new char[INIT_SZ];
+    top  = -1;
   }
 
   public String toString() {
