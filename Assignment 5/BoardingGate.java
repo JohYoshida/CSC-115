d4aea6a74b479964204d1afb6114f8bbab361f43
Joh Yoshida <johyoshida@gmail.com>
2020-07-08 21:02:26 -0700

    Initial commit

---
+++ Assignment 5/BoardingGate.java
@@ -0,0 +1,67 @@
+/*
+ * represents a transit boarding gate (train, airplane, etc.)
+ */
+
+public class BoardingGate {
+  private PriorityQueue passengers;
+
+  /*
+   * constructor that initializes passengers to a default-sized priority queue
+   */
+  public BoardingGate() {
+    // TODO: implement this
+  }
+
+  /*
+   * constructor that initializes passengers to a priority queue of given size
+   */
+  public BoardingGate(int size) {
+    // TODO: implement this
+  }
+
+  /* addPassenger
+   * PURPOSE:
+   *  add given Passenger p to passengers and
+   *  prints a notification message if passengers is full
+   *
+   * PARAMETERS:
+   *  Passenger p - Passenger to be added
+   *
+   * RETURNS:
+   *  None.
+   */
+  public void addPassenger(Passenger p) {
+    // TODO: implement this
+  }
+
+  /* numPassengersWaiting
+   * PURPOSE:
+   *  returns the number of Passengers in passengers waiting to board
+   *
+   * PARAMETERS:
+   *  None.
+   *
+   * RETURNS:
+   *  int - number of passengers waiting to board
+   */
+  public int numPassengersWaiting() {
+    // TODO: implement this
+    return -1;             // so it compiles
+  }
+
+  /* nextPassenger
+   *
+   * PURPOSE:
+   *  removes and returns the next Passenger from passengers
+   *
+   * PARAMETERS:
+   *  None.
+   *
+   * RETURNS:
+   *  Passenger - the next Passenger, null if there is no more Passengers.
+   */
+  public Passenger nextPassenger() {
+    // TODO: implement this
+    return null;             // so it compiles
+  }
+}
---
+++ Assignment 5/HeapPriorityQueue.java
@@ -0,0 +1,66 @@
+/*
+ * HeapPriorityQueue.java
+ *
+ * An implementation of a minimum PriorityQueue using a heap.
+ * based on the implementation in "Data Structures and Algorithms
+ * in Java", by Goodrich and Tamassia
+ *
+ * This implementation will throw a Runtime, HeapEmptyException
+ *	if the heap is empty and removeLow is called.
+ *
+ * This implementation will throw a Runtime, HeapFullException
+ *	if the heap is full and insert is called.
+ *
+ */
+
+public class HeapPriorityQueue implements PriorityQueue {
+  protected final static int DEFAULT_SIZE = 10000;
+
+  protected Comparable[] storage;
+  protected int currentSize;
+
+  /*
+   * Constructor that initializes the array to hold DEFAULT_SIZE elements
+   */
+  public HeapPriorityQueue() {
+    storage     = new Comparable[DEFAULT_SIZE];
+    currentSize = 0;
+  }
+
+  /*
+   * Constructor that initializes the array to hold size elements
+   */
+  public HeapPriorityQueue(int size) {
+    //TODO: Implement this
+  }
+
+  public void insert(Comparable element) {
+    //TODO: Implement this
+  }
+
+  public Comparable removeMin() {
+    //TODO: Implement this
+    return null;             // so it compiles
+  }
+
+  public boolean isEmpty() {
+    //TODO: Implement this
+    return false;             // so it compiles
+  }
+
+  public int size() {
+    //TODO: Implement this
+    return -1;             // so it compiles
+  }
+
+  public String toString() {
+    String s   = "";
+    String sep = "";
+
+    for (int i = 0; i < currentSize; i++) {
+      s  += sep + storage[i];
+      sep = " ";
+    }
+    return s;
+  }
+}
