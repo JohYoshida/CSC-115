6e5fcdd55f4edf4cfe943a74999a78d76cd29b74
Joh Yoshida <johyoshida@gmail.com>
2020-07-28 14:39:04 -0700

    Part I

--- Assignment 7/HashMap.java
+++ Assignment 7/HashMap.java
@@ -47,9 +47,16 @@ public class HashMap <K extends Comparable <K>, V> implements Map <K, V> {
     // gets the index in the table this key should be in
     int index = Math.abs(key.hashCode()) % tableSize;
 
-    // TODO... complete this method
-
-
+    List <Entry <K, V> > chain = table.get(index);
+    if (chain.isEmpty() == false) {
+      Iterator <Entry <K, V> > iter = chain.iterator();
+      while (iter.hasNext()) {
+        Entry <K, V> element = iter.next();
+        if (element.getKey().equals(key)) {
+          return true;
+        }
+      }
+    }
     return false;
   }
 
@@ -57,17 +64,31 @@ public class HashMap <K extends Comparable <K>, V> implements Map <K, V> {
     // gets the index in the table this key should be in
     int index = Math.abs(key.hashCode()) % tableSize;
 
-    // TODO... complete this method
-
-
-
+    List <Entry <K, V> > chain = table.get(index);
+    if (chain.isEmpty() == false) {
+      Iterator <Entry <K, V> > iter = chain.iterator();
+      while (iter.hasNext()) {
+        Entry <K, V> element = iter.next();
+        if (element.getKey().equals(key)) {
+          return element.getValue();
+        }
+      }
+    }
     throw new KeyNotFoundException();
   }
 
   public List <Entry <K, V> > entryList() {
     List <Entry <K, V> > l = new LinkedList <Entry <K, V> >();
-    // TODO... complete this method
-
+    for (int i = 0; i < tableSize; i++) {
+      List <Entry <K, V> > chain = table.get(i);
+      if (chain.isEmpty() == false) {
+        Iterator <Entry <K, V> > iter = chain.iterator();
+        while (iter.hasNext()) {
+          Entry <K, V> element = iter.next();
+          l.add(element);
+        }
+      }
+    }
     // Tip: you will need to iterate through each index in the table (and each index holds a list)
     //      you will THEN need to iterate through each element in the linked list chain at a
     //      specific index and add each element to l
@@ -80,7 +101,27 @@ public class HashMap <K extends Comparable <K>, V> implements Map <K, V> {
     // gets the index in the table this key should be in
     int index = Math.abs(key.hashCode()) % tableSize;
 
-    // TODO... complete this method
+    Entry <K, V> entry         = new Entry <K, V>(key, value);
+    List <Entry <K, V> > chain = table.get(index);
+    if (chain.isEmpty()) {
+      chain.add(entry);
+      count++;
+    }
+    else {
+      boolean put = false;
+      Iterator <Entry <K, V> > iter = chain.iterator();
+      while (iter.hasNext()) {
+        Entry <K, V> element = iter.next();
+        if (element.getKey().equals(key)) {
+          element.setValue(value);
+          put = true;
+          break;
+        }
+      }
+      if (put == false) {
+        chain.add(entry);
+      }
+    }
 
     // if key is found, update value.  if key is not found add a new Entry with key,value
     // The tester expects that you will add the newest Entry to the END of the list
